8e0cad3d0e7b9df64b1497bf1d79acb1b45ba0f0
Douglas Lima <douglas.lima.queiroz@gmail.com>
2024-04-18 14:00:49 -0300

    compilado completo

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,20 @@
+
+import ListaTemas from '../../components/temas/listaTemas/ListaTemas';
+
+function Home() {
+ 
+
+  return (
+    <div className='flex justify-center items-center'>
+      <div>
+        {/* <h2 className="text-slate-900 text-5xl  my-4">Logar</h2>
+        <h2 className="text-slate-900 text-4xl ">Ola user : {nome}</h2> */}
+        <ListaTemas/>
+      
+      </div>
+
+    </div>
+  );
+}
+
+export default Home;
