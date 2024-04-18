3ba2faf11837ea351ece12e4896c729acff27e61
Douglas Lima <douglas.lima.queiroz@gmail.com>
2024-04-18 14:03:28 -0300

    up

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,42 +1,44 @@
-import { useContext } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { AuthContext } from '../../contexts/AuthContext'
-
-
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { usuario, handleLogout } = useContext(AuthContext)
+  const { usuario, handleLogout } = useContext(AuthContext);
 
   function logout() {
-      handleLogout()
-      alert('Usuário deslogado com sucesso')
-      navigate('/logar')
+    handleLogout();
+    alert("Usuário deslogado com sucesso");
+    navigate("/logar");
   }
-
-
-  
 
   return (
     <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+      <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
+        <div className="container flex justify-between text-lg">
+          <Link to="/home" className="text-2xl font-bold uppercase">
+            Blog Pessoal
+          </Link>
 
-            <div className='flex gap-4'>
-            <div className='hover:underline'>Postagens</div>
-            <Link to='/tema' className='hover:underline'>Temas</Link>
-            <Link to="/cadastroTema" className='hover:underline'>Cadastrar tema</Link>
-              <div className='hover:underline'>Perfil</div>
-              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
-             
-            </div>
+          <div className="flex gap-4">
+            <div className="hover:underline">Postagens</div>
+            <Link to="/tema" className="hover:underline">
+              Temas
+            </Link>
+            <Link to="/cadastroTema" className="hover:underline">
+              Cadastrar tema
+            </Link>
+            <div className="hover:underline">Perfil</div>
+            <Link to="" onClick={logout} className="hover:underline">
+              Sair
+            </Link>
           </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
