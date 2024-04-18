a4de01ed9940359f581f3d1387a3c53e91159e3e
Douglas Lima <douglas.lima.queiroz@gmail.com>
2024-04-18 14:10:27 -0300

    non used react

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
 
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/login/Login';
-import Cadastro from './pages/cadastro/Cadastro';
-import Home from './pages/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
-import ListaTemas from './components/temas/listaTemas/ListaTemas';
-import FormularioTema from './components/temas/formularioTema/FormularioTema';
-import DeletarTema from './components/temas/deletarTema/DeletarTema';
-
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./components/temas/listaTemas/ListaTemas";
+import FormularioTema from "./components/temas/formularioTema/FormularioTema";
+import DeletarTema from "./components/temas/deletarTema/DeletarTema";
 
 function App() {
   return (
     <>
-    <AuthProvider>
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
-          <div className='min-h-[80vh]'>
+          <div className="min-h-[80vh]">
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/logar" element={<Login />} />
@@ -33,7 +31,7 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
-        </AuthProvider>
+      </AuthProvider>
     </>
   );
 }
